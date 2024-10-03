96a274753ce89b22bf891c447fc5af1a95d179f8
João Vitor <joaovitorportosales46@gmail.com>
2024-10-03 15:45:24 -0300

    Feat: Create get all planets methods

--- src/routes/planetas.routes.js
+++ src/routes/planetas.routes.js
@@ -1,36 +1,30 @@
 import { Router } from "express";
 
-const filmesRoutes = Router();
+const planetasRoutes = Router();
 
-let filmesMarcantes = [
+let planetas = [
     {
-        id: (Math.floor(Math.random() * 99) + 1),
-        titulo: "Meu Amigo Totoro",
-        genero: "Animação",
-        emCartaz: false,
+        id: (Math.floor(Math.random() * 999999) + 1),
+        nome: "Dev",
+        temperatura: 13.0,
+        agua: false, //Indicação de existência de água
+        atm: [
+            "JS",
+            "Node",
+            "VS",
+            "Code",
+        ],
     },
-    {  
-        id:(Math.floor(Math.random() * 99) + 1),
-        titulo: "Deadpool 3",
-        genero: "Ação e Comédia",
-        emCartaz: true,
-    },
-    {
-        id:(Math.floor(Math.random() * 99) + 1),
-        titulo: "Meu Malvado Favorito 4",
-        genero: "Animação",
-        emCartaz: true,
-    }
 ]
 
-//Rota para buscar todo os elementos do array filmesMarcantes
-filmesRoutes.get("/", (req, res) => {
-    return res.status(200).send(filmesMarcantes);
+//Rota para buscar todo os elementos do array planetas
+planetasRoutes.get("/", (req, res) => {
+    return res.status(200).send(planetas);
 })
 
 //Rota para criar um novo filme 
-filmesRoutes.post("/", (req, res) => {
-    const {titulo, genero, emCartaz } = req.body;
+planetasRoutes.post("/", (req, res) => {
+    const { titulo, genero, emCartaz } = req.body;
 
     const novoFilme = {
         id: Number(Math.floor(Math.random() * 99) + 1),
@@ -39,17 +33,17 @@ filmesRoutes.post("/", (req, res) => {
         emCartaz,
     };
 
-    filmesMarcantes.push(novoFilme);
-    return res.status(201).send(filmesMarcantes);
+    planetas.push(novoFilme);
+    return res.status(201).send(planetas);
 })
 
-//Rota para buscar um elemento específico do array filmesMarcantes
-filmesRoutes.get("/:id", (req, res) => {
+//Rota para buscar um elemento específico do array planetas
+planetasRoutes.get("/:id", (req, res) => {
     const { id } = req.params;
 
     //console.log(id);
 
-    const filme = filmesMarcantes.find((movie) => movie.id === Number(id));
+    const filme = planetas.find((movie) => movie.id === Number(id));
 
     //console.log(guloseima);
 
@@ -61,10 +55,10 @@ filmesRoutes.get("/:id", (req, res) => {
 })
 
 //Rota para editar um filme
-filmesRoutes.put("/:id", (req, res) => {
+planetasRoutes.put("/:id", (req, res) => {
     const { id } = req.params;
 
-    const filme = filmesMarcantes.find((movie) => movie.id === Number(id));
+    const filme = planetas.find((movie) => movie.id === Number(id));
 
     //console.log(guloseima);
 
@@ -80,28 +74,28 @@ filmesRoutes.put("/:id", (req, res) => {
     filme.emCartaz = emCartaz;
 
     return res.status(200).send({
-        message:"Filme atualizado!",
+        message: "Filme atualizado!",
         filme,
     });
 });
 
 //Rota para deletar um filme 
-filmesRoutes.delete("/:id", (req, res) => {
+planetasRoutes.delete("/:id", (req, res) => {
     const { id } = req.params;
 
-    const filme = filmesMarcantes.find((movie) => movie.id === Number(id));
+    const filme = planetas.find((movie) => movie.id === Number(id));
 
-    
+
     if (!filme) {
         return res.status(404).send({ message: "Filme não encontrado!" });
     }
-    
-    filmesMarcantes = filmesMarcantes.filter((movie) => movie.id !== Number(id));
 
-    return res.status(200).send ({
+    planetas = planetas.filter((movie) => movie.id !== Number(id));
+
+    return res.status(200).send({
         message: "Filme deletado!",
         filme,
     });
 });
 
-export default filmesRoutes;
+export default planetasRoutes;
